0299eec9fef186db1af84799f09e3b538bb3df04
Tanja Rohlfs <tanja@georepublic.de>
2021-06-25 16:57:41 +0900

    minor changes. css: typography changes margin / padding changes content: removed "roadmap" because it was very oudated. Updated text for "how it works" slightly. Rearranged content order.

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,72 +27,53 @@ function Home() {
           </div>
         </header>
         <main className="landing">
-         <section className="landing__values">
-              <h2>Our values</h2>
-              <ul className="landing__values__list">
+          <section className="landing__how">
+              <h2>How it works</h2>
+              <ul className="landing__how__list">
                 <li>
-                  <img src={useBaseUrl('img/tanja/cc-by-nc-sa/icons/icon_lock.svg')} />
-                  <h3>Privacy by Design</h3>
-                  <p>Consento provides 100% privacy.</p>
+                  <h3>Encryption</h3>
+                  <img src={useBaseUrl('img/tanja/cc-by-nc-sa/illustrations/04-how-to-1.svg')} />
+                  <p>Data will be secured with an encryption key.</p>
                 </li>
                 <li>
-                  <img src={useBaseUrl('img/tanja/cc-by-nc-sa/icons/icon_empowerment.svg')} />
-                  <h3>Empowerment</h3>
-                  <p>Consento makes you keep control over our data.</p>
+                  <h3>Shared key</h3>
+                  <img src={useBaseUrl('img/tanja/cc-by-nc-sa/illustrations/04-how-to-2.svg')} />
+                  <p>The key is split in unique parts & shared with people you trust.</p>
                 </li>
                 <li>
-                  <img src={useBaseUrl('img/tanja/cc-by-nc-sa/icons/icon_human centric.svg')} />
-                  <h3>Human centric</h3>
-                  <p>Consento builds on trusted human relationship.</p>
+                  <h3>Unlocking</h3>
+                  <img src={useBaseUrl('img/tanja/cc-by-nc-sa/illustrations/04-how-to-3.svg')} />
+                  <p>'Consent' among all keyholders required to unlock the data.</p>
                 </li>
                 <li>
-                  <img src={useBaseUrl('img/tanja/cc-by-nc-sa/icons/icon_transparency.svg')} />
-                  <h3>Transparency</h3>
-                  <p>Consento is 100% FOSS.</p>
-                </li>
-                <li>
-                  <img src={useBaseUrl('img/tanja/cc-by-nc-sa/icons/icon_diversity.svg')} />
-                  <h3>Diversity</h3>
-                  <p>Consento empowers small businesses.</p>
+                  <h3>Private network</h3>
+                  <img src={useBaseUrl('img/tanja/cc-by-nc-sa/illustrations/04-how-to-4.svg')} />
+                  <p>Keep control over who has access.</p>
                 </li>
               </ul>
             </section>
           <section className="landing__video">
+         
+            <div class="landing__text">
+              <h2>Feel confident to handle sensitive data</h2>
+              <p>The many potenatials brought by the digital transformation come along with data privacy challenges. These challenges lie less in technical systems than in the stress of managing confidential data collectively. Consento empowers professionals and SMEs to build their private decentralized, passwordless multi-factor authentication hub. </p></div>
+
             <div className="landing__vimeo">
+
               <iframe
                 src="https://player.vimeo.com/video/498249054?title=0&byline=0&portrait=0"
                 style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
                 frameBorder="0"
                 allow="autoplay; fullscreen; picture-in-picture" />
             </div>
-            <p className="landing__text landing__text__full">The many potenatials brought by the digital transformation come along with data privacy challenges. These challenges lie less in technical systems than in the stress of managing confidential data collectively. Consento empowers professionals and SMEs to build their private decentralized, passwordless multi-factor authentication hub. Our apps turn your devices into keys. The access to confidential data becomes collectively secured. The responsibility of handling confidential access is shared.</p>
+            
+      
           </section>
-          <section className="landing__solution">
-            <h2>Our solution</h2>
-            <div className="landing__image">
-              <img src={useBaseUrl('img/consento/cc-by-nc-nd/phone-mockup.svg')} />
-            </div>
-            <div className="landing__text landing__text__right">
-              <p>Consento app enables you to build your custom Human-Factor Authentication in your organization: </p>
-              <ul>
-                <li><strong>Password-less</strong>, Consento removes the target of hacks or the risk of mishandling passwords.</li>
-                <li><strong>Cloud-free</strong>, Consento removes the middle man and the authentication process remains private to its users only without the need of a centralized service. </li>
-                <li><strong>Co-responsive</strong>, Consento makes authentication more secure through partial keys distributed within your organization and to your suppliers or customers. No single user is left as a potential point of failure of the system as a whole.</li>
-                <li><strong>"Shared-secrets"</strong> are quantum-proof.</li>
-              </ul>
-              <p>Now organizations can manage the access and sharing of confidential operations with a peace of mind. They remain in control of who has access, and they don’t bear the liability of data privacy alone.</p>
-
-              <a className="button__full" href="/download">Be the first to use it</a>
-              <a className="button__full" target="_blank" href="https://twitter.com/consentoorg">Get notified about releases</a>
-            </div>
-          </section>
-
           <section className="landing__usecase">
             <div className="landing__text landing__text__full">
               <h2>Use cases</h2>
               <p>Here is a short text to present the key use-cases on which the Consento team is actively working at the moment, developping the code, the screens and the partnerships.</p>
-            </div>
-            
+            </div> 
             <ul className="landing__usecase__list landing__values__list">
             <li>
             <figure className="kg-card kg-bookmark-card">
@@ -148,6 +129,25 @@ function Home() {
             </li>
             </ul>
           </section>
+          <section className="landing__solution">
+            <h2>Our solution</h2>
+            <div className="landing__image">
+              <img src={useBaseUrl('img/consento/cc-by-nc-nd/phone-mockup.svg')} />
+            </div>
+            <div className="landing__text landing__text__right">
+              <p>Consento app enables you to build your custom Human-Factor Authentication in your organization: </p>
+              <ul>
+                <li><strong>Password-less</strong>, Consento removes the target of hacks or the risk of mishandling passwords.</li>
+                <li><strong>Cloud-free</strong>, Consento removes the middle man and the authentication process remains private to its users only without the need of a centralized service. </li>
+                <li><strong>Co-responsive</strong>, Consento makes authentication more secure through partial keys distributed within your organization and to your suppliers or customers. No single user is left as a potential point of failure of the system as a whole.</li>
+                <li><strong>"Shared-secrets"</strong> are quantum-proof.</li>
+              </ul>
+              <p>Now organizations can manage the access and sharing of confidential operations with a peace of mind. They remain in control of who has access, and they don’t bear the liability of data privacy alone.</p>
+
+              <a className="button__full" href="/download">Be the first to use it</a>
+              <a className="button__full" target="_blank" href="https://twitter.com/consentoorg">Get notified about releases</a>
+            </div>
+          </section>
           <section className="landing__technology">
             <div className="landing__text landing__text__left">
               <h2>The technology</h2>
@@ -180,79 +180,37 @@ function Home() {
 }`}</CodeBlock>
               </div>
             </section>
-            <section className="landing__how">
-              <h2>How does it work</h2>
-              <ul className="landing__how__list">
+          <section className="landing__values">
+              <h2>Our values</h2>
+              <ul className="landing__values__list">
                 <li>
-                  <h3>Encryption</h3>
-                  <img src={useBaseUrl('img/tanja/cc-by-nc-sa/illustrations/04-how-to-1.svg')} />
-                  <p>Strong encryption on the spot.</p>
+                  <img src={useBaseUrl('img/tanja/cc-by-nc-sa/icons/icon_lock.svg')} />
+                  <h3>Privacy by Design</h3>
+                  <p>Consento provides 100% privacy.</p>
                 </li>
                 <li>
-                  <h3>Shared key</h3>
-                  <img src={useBaseUrl('img/tanja/cc-by-nc-sa/illustrations/04-how-to-2.svg')} />
-                  <p>Key split in unique parts & shared with people you trust.</p>
+                  <img src={useBaseUrl('img/tanja/cc-by-nc-sa/icons/icon_empowerment.svg')} />
+                  <h3>Empowerment</h3>
+                  <p>Consento makes you keep control over our data.</p>
                 </li>
                 <li>
-                  <h3>Unlocking</h3>
-                  <img src={useBaseUrl('img/tanja/cc-by-nc-sa/illustrations/04-how-to-3.svg')} />
-                  <p>'Consent' required to unlock.</p>
+                  <img src={useBaseUrl('img/tanja/cc-by-nc-sa/icons/icon_human centric.svg')} />
+                  <h3>Human centric</h3>
+                  <p>Consento builds on trusted human relationship.</p>
                 </li>
                 <li>
-                  <h3>Private network</h3>
-                  <img src={useBaseUrl('img/tanja/cc-by-nc-sa/illustrations/04-how-to-4.svg')} />
-                  <p>Keep control over who has access.</p>
+                  <img src={useBaseUrl('img/tanja/cc-by-nc-sa/icons/icon_transparency.svg')} />
+                  <h3>Transparency</h3>
+                  <p>Consento is 100% FOSS.</p>
+                </li>
+                <li>
+                  <img src={useBaseUrl('img/tanja/cc-by-nc-sa/icons/icon_diversity.svg')} />
+                  <h3>Diversity</h3>
+                  <p>Consento empowers small businesses.</p>
                 </li>
               </ul>
             </section>
-            <section className="landing__roadmap">
-              <h2>Consento roadmap</h2>
-              <div className="landing__image landing__image__full">
-                <img src={useBaseUrl('img/tanja/cc-by-nc-sa/illustrations/human-centric@1x.png')} />
-              </div>
-              <figure className="landing__tasklist landing__tasklist__backlog">
-                <h3>Backlog</h3>
-                <ul>
-                  <li>
-                    <h4>Vault sharing</h4>
-                    <p>Enabling users to share the content of their vault with selected users.</p>
-                  </li>
-                  <li>
-                    <h4>iOS mobile app</h4>
-                    <p>Create application for iOS Appstore.</p>
-                  </li>
-                  <li>
-                    <h4>Desktop app</h4>
-                    <p>Developing a desktop client.</p>
-                  </li>
-                  <li>
-                    <h4>Policies editing</h4>
-                    <p>Defining a default policy.md file at the root of vaults.</p>
-                  </li>
-                </ul>
-              </figure>
-              <figure className="landing__tasklist landing__tasklist__progress">
-                <h3>In Progress</h3>
-                <ul>
-                  <li>
-                    <h4>Android mobile app</h4>
-                    <p>Finalizing application for Google Play store.</p>
-                  </li>
-                  <li>
-                    <h4>File Upload</h4>
-                    <p>Enabling to Upload various file types in vault.</p>
-                  </li>
-                </ul>
-              </figure>
-              <div className="landing__text landing__text__right">
-                <p>Consento is proudly part of the <a href="https://ledgerproject.eu/" target="_blank">LEDGER</a> program, and currently working on the MVP.</p>
-                <p>We have a lot in the pipeline, feel free to give a hand.</p>
-                <p>Sign up to stay updated of our progress and next releases.</p>
-                <a className="button__full" href="https://github.com/consento-org/">Contribute</a>
-                <a className="button__full" href="https://mailchi.mp/54ed51f62b3d/consento-updates-signup">Stay updated</a>
-              </div>
-            </section>
-            <section className="landing__support">
+          <section className="landing__support">
               <h2>Support the Consento Initiative</h2>
               <div className="landing__image landing__image__full">
                 <img src={useBaseUrl('img/tanja/cc-by-nc-sa/illustrations/diversity.svg')} />
